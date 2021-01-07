f5608c6efc19a17b64968c216dcaed76b8c0368e
Damian Perez <dajpes@gmail.com>
2021-01-07 16:22:49 -0500

    feat: Created a Pokemon Name Component

---
+++ src/components/pokemon/PokemonName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>
+    <span class="">{{ name }}</span>
+    <p>
+        {{inputSearch}}
+    </p>
+  </h1>
+</template>
+
+<script>
+import PokemonHandler from "@/composables/Pokemons";
+export default {
+  props: {
+    name: { type: String, required: true },
+  },
+  setup() {
+    const { inputSearch } = PokemonHandler();
+    return {inputSearch};
+  },
+};
+</script>
+
+
