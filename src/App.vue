a02fd54cd1dd5b9305170c03a34227067a5cae15
Damian Perez <dajpes@gmail.com>
2021-01-07 14:48:07 -0500

    feat: Added new functionallity for pokemon Search

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <label for="maxCP" class="max-cp">
-      <input type="checkbox" id="maxCP" v-model="pokemon.maxPoints" />
+      <input type="checkbox" id="maxCP" v-model="pokemonState.maxPoints" />
       <small>Maximum Combat Points</small>
     </label>
     <input
       type="text"
       className="input"
       placeholder="Pokemon or type"
-      v-model="pokemon.input"
+      v-model="pokemonState.inputSearch"
       @input="searchPokemons"
     />
     <!-- {loading && <div className="loader"></div>} -->
@@ -21,35 +21,57 @@
 import PokemonSuggestion from "./components/pokemon/PokemonSuggestion.vue";
 import { reactive } from "vue";
 
-
 const URL_PATH =
   "https://gist.githubusercontent.com/bar0191/fae6084225b608f25e98b733864a102b/raw/dea83ea9cf4a8a6022bfc89a8ae8df5ab05b6dcc/pokemon.json";
 
 export default {
   setup() {
-    const pokemon = reactive({
+    const pokemonState = reactive({
       pokemonList: [],
       maxPoints: false,
-      input: "",
+      inputSearch: "",
+      pokemonListResult: [],
     });
 
-
     const searchPokemons = () => {
-      console.log('Me esta escribiendo');
+      console.log("Me esta escribiendo");
+      // console.log('Me esta escribiendo',pokemonState.pokemonList);
+
+      const getPokemons = pokemonState.pokemonList.filter(
+        ({ Name: pokemonName, Types: types }) => {
+          // console.log("El poke t: ", type,[...type]);
+          // pokemon.Name.toLowerCase().includes(pokemon.toLowerCase())
+          const searchPokemonByType = types.map((type) =>
+            type.toLowerCase().startsWith(pokemonState.inputSearch)
+          );
+          // return pokemonName.toLowerCase().includes(pokemonState.inputSearch)
+          return (
+            pokemonName.toLowerCase().includes(pokemonState.inputSearch) ||
+            searchPokemonByType.length
+          );
+        }
+      );
+      pokemonState.pokemonListResult =
+        getPokemons.length > 3 ? getPokemons.slice(0, 4) : getPokemons;
+      console.log("Veamoooos el result", pokemonState.pokemonListResult);
     };
 
-    const fetchPokemons =  () => {
-      const fetchPok = fetch(URL_PATH).then(res=>res.json())
-      pokemon.pokemonList = fetchPok
+    const fetchPokemons = async () => {
+      try {
+        const fetchPok = await fetch(URL_PATH).then((res) => res.json());
+        pokemonState.pokemonList = fetchPok;
+      } catch (e) {
+        console.log("Error while Fetching Pokemons: ", e);
+        throw e;
+      }
     };
 
     fetchPokemons();
 
-
     return {
       PokemonSuggestion,
-      pokemon,
-      searchPokemons
+      pokemonState,
+      searchPokemons,
     };
   },
 };
