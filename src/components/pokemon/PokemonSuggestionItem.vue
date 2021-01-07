cfef38bea12bd6211284c1257d99702c16aa7039
Damian Perez <dajpes@gmail.com>
2021-01-07 15:57:01 -0500

    fix: Imported Composable Used PokemonType Component

---
+++ src/components/pokemon/PokemonSuggestionItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <img :src="pokemon.img" :alt="pokemon.About" />
+    <div className="info">
+        {{pokemon.Types}}
+      <h1>
+        <span style="margin-left: 10px">MaxCp:{{ pokemon.MaxCP || "0" }}</span>
+      </h1>
+      <PokemonType
+        v-for="type in pokemon.Types"
+        :key="`${type}_${pokemon.Number}`"
+        :pokemonType="type"
+      />
+    </div>
+  </li>
+</template>
+
+<script>
+import PokemonType from "./PokemonType";
+export default {
+  props: {
+    pokemon: { type: Object, required: true },
+  },
+  setup() {
+    return { PokemonType };
+  },
+};
+</script>
