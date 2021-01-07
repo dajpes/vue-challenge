d8bc38152844be1e472312f01bfdd1a5b6a4c0e3
Damian Perez <dajpes@gmail.com>
2021-01-07 15:56:29 -0500

    feat: Created Pokemon type Component

---
+++ src/components/pokemon/PokemonType.vue
@@ -0,0 +1,18 @@
+<template>
+  <span :class="`type ${pokemonState.type}`">{{ pokemonType }}</span>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+export default {
+  props: {
+    pokemonType: { type: String, default: "Normal" },
+  },
+  setup(props) {
+    const pokemonState = reactive({
+      type: computed(() => props.pokemonType.toLowerCase()),
+    });
+    return { pokemonState };
+  },
+};
+</script>
