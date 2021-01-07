d1eb2af978b94837141cbf19c39df7babfd5d12b
Damian Perez <dajpes@gmail.com>
2021-01-07 14:20:19 -0500

    feat: First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <label for="maxCP" class="max-cp">
+      <input type="checkbox" id="maxCP" v-model="pokemon.maxPoints" />
+      <small>Maximum Combat Points</small>
+    </label>
+    <input
+      type="text"
+      className="input"
+      placeholder="Pokemon or type"
+      v-model="pokemon.input"
+      @input="searchPokemons"
+    />
+    <!-- {loading && <div className="loader"></div>} -->
+    <!-- <Suggestions pokemonSuggestions={pokemonList} querySearch={pokemon} /> -->
+    <PokemonSuggestion />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonSuggestion from "./components/pokemon/PokemonSuggestion.vue";
+import { reactive } from "vue";
+
+
+const URL_PATH =
+  "https://gist.githubusercontent.com/bar0191/fae6084225b608f25e98b733864a102b/raw/dea83ea9cf4a8a6022bfc89a8ae8df5ab05b6dcc/pokemon.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    const pokemon = reactive({
+      pokemonList: [],
+      maxPoints: false,
+      input: "",
+    });
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+    const searchPokemons = () => {
+      console.log('Me esta escribiendo');
+    };
+
+    const fetchPokemons =  () => {
+      const fetchPok = fetch(URL_PATH).then(res=>res.json())
+      pokemon.pokemonList = fetchPok
+    };
+
+    fetchPokemons();
+
+
+    return {
+      PokemonSuggestion,
+      pokemon,
+      searchPokemons
+    };
+  },
+};
+</script>
---
+++ src/components/pokemon/PokemonSuggestion.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        hello woooooooooords
+    </div>
+</template>
+
+<script setup>
+
+</script>
