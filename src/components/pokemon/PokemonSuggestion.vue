6a43308793ca92eb105df65cb5af66eeba8c70ca
Damian Perez <dajpes@gmail.com>
2021-01-07 15:58:17 -0500

    fix: Added logic to View!

--- src/components/pokemon/PokemonSuggestion.vue
+++ src/components/pokemon/PokemonSuggestion.vue
@@ -1,9 +1,34 @@
 <template>
-    <div>
-        hello woooooooooords
-    </div>
+  <div>
+    <ul v-if="pokemonListResult.length" className="suggestions">
+      <PokemonSuggestionItem
+        v-for="pokemon in pokemonListResult"
+        :key="+pokemon.Number"
+        :pokemon="pokemon"
+      />
+    </ul>
+    <ul v-else>
+      <li>
+        <img
+          src="https://cyndiquil721.files.wordpress.com/2014/02/missingno.png"
+          alt=""
+        />
+        <div className="info">
+          <h1 className="no-results">No results</h1>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup>
+<script>
+import PokemonHandler from "@/composables/Pokemons";
+import PokemonSuggestionItem from "./PokemonSuggestionItem";
+export default {
+  setup() {
+    const { pokemonListResult } = PokemonHandler();
 
+    return { PokemonSuggestionItem, pokemonListResult };
+  },
+};
 </script>
