696d2a038964e8d3d36aa48865175858ca615db4
Damian Perez <dajpes@gmail.com>
2021-01-08 08:50:27 -0500

    feat: Added a Constant config, to store unchangeable values

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+const FETCH = {
+  POKEMON_FETCH: {
+    value:
+      'https://gist.githubusercontent.com/bar0191/fae6084225b608f25e98b733864a102b/raw/dea83ea9cf4a8a6022bfc89a8ae8df5ab05b6dcc/pokemon.json',
+  },
+};
+
+export { FETCH };
